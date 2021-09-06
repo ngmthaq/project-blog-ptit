c04ce896bb7b8081fb838caf50087c1d4f28d294
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2021-09-06 16:14:45 +0700

    add favicon

--- .history/views/parts/__head_20210906112507.php
+++ .history/views/parts/__head_20210906112507.php
@@ -1,4 +1,4 @@
-<link rel="shortcut icon" href="./public/assets/img/favicon.png" type="image/x-icon">
+<link rel="shortcut icon" href="./public/assets/img/Homestay.png" type="image/x-icon">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
